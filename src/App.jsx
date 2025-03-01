fbd253b4e0506fb039850a870d2d6c4d403a4b3a
ren <93995954+cientificoqq@users.noreply.github.com>
2025-03-01 14:15:41 +0200

    ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-// App.js
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 
 import Header from "./components/Header";
 import RouterLayout from "./components/router-layout";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <BrowserRouter>
-      <Header />
-      <RouterLayout />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+        <RouterLayout />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import { AppBar, Button, Container, Toolbar } from "@mui/material";
+import { AppBar, Button, Container, IconButton, Toolbar } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
-
-const Header = () => {
+import { LightMode, DarkMode } from "@mui/icons-material";
+const Header = ({ darkMode, setDarkMode }) => {
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -17,6 +17,9 @@ const Header = () => {
               To Do List
             </Button>
           </Link>
+          <IconButton onClick={() => setDarkMode(!darkMode)} color="inherit">
+            {darkMode ? <DarkMode /> : <LightMode />}
+          </IconButton>
         </Toolbar>
       </Container>
     </AppBar>
